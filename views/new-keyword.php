54ff9454ece4872e0945c0ca9502071de2262b91
leomar <leomar.osorio@euroland.com>
2019-08-14 20:35:41 +0800

    removed the validation error on the new search term form

--- views/new-keyword.php
+++ views/new-keyword.php
@@ -1,7 +1,7 @@
 
 
-<div style="color:red; width: 100%; text-align: center;margin-top: 30px;"><?php echo validation_errors(); ?></div>
-
+<!-- <div style="color:red; width: 100%; text-align: center;margin-top: 30px;"><?php echo validation_errors(); ?></div>
+ -->
 <div class="new-keyword-section">
 
 <h3><?php echo $message; ?></h3>
@@ -9,29 +9,30 @@
 	<form action="http://localhost:8002/keywords/index.php/keywords/add_keyword" method="POST">
 
 	    <label for="keyword">Keyword</label>
-	    <input type="input" name="keyword" /><br />
+	    <input required type="input" name="keyword" /><br />
 
 	    <label for="language">Language</label>
-	    <select name="language">
+	    <select required name="language">
 	    	<?php foreach ($languages as $language): ?>
 	    	<option value="<?php echo $language['id']; ?>"> <?php echo $language['language']; ?> </option>
 	    	<?php endforeach; ?>
 	    </select><br />
 
 	    <label for="report_type">Report Type</label>
-	    <select name="report_type">
+	    <select required name="report_type">
 	    	<?php foreach ($report_types as $report_type): ?>
 	    	<option value="<?php echo $report_type['id']; ?>"> <?php echo $report_type['type_name']; ?> </option>
 	    	<?php endforeach; ?>
 	    </select><br />
 
 	    <label for="parent">Parent Type</label>
-	    <select name="parent">
+	    <select required name="parent">
 	    	<?php foreach ($parents as $parent): ?>
 	    	<option value="<?php echo $parent['id']; ?>"> <?php echo $parent['parent_name']; ?> </option>
 	    	<?php endforeach; ?>
 	    </select><br />
 
 	    <input type="submit" name="submit" value="Create news item" />
-
+	    <br><br>
+	    <a href="javascript:history.back()">Go Back</a>
 	</form>
